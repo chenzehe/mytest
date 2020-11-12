f143f78e9373c7b34f41092eeea9ee153f5c595e
chenzehe <chenzehe@qiakr.com>
2020-11-12 16:14:11 +0800

    sort

---
+++ src/main/java/com/chenzehe/test/upload/SortNum.java
@@ -0,0 +1,21 @@
+package com.chenzehe.test.upload;
+
+import java.util.Arrays;
+
+/**
+ * 任给一个数组，其中只有一个元素是单独出现，其他是成对出现，输出单独的元素
+ * 例如：输入：{2，2，1，1，4，4，7} 输出：7
+ * @author hljuczh@163.com
+ */
+public class SortNum {
+    public static void main(String[] args) {
+
+    }
+
+    public static  String findSortNum(Integer[] sortNum,Integer num){
+        //1 算出数组中每个元素在本数据中的出现次数放到map中保存起来
+        //Arrays.stream(sortNum).map()
+        //输出再现次数的数组
+        return null;
+    }
+}
